570b602832979da8287361b9c1eff0a25f0e2579
tinygrox <tinygrox@gmail.com>
2024-10-08 20:11:31 +0800

    Harmony 汉化试行 - Where Can I Go

---
+++ Source/KSP_Chinese_Patches/CNPatchInit.cs
@@ -0,0 +1,43 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+namespace KSP_Chinese_Patches
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, once: true)]
+    public class CNPatchInit : MonoBehaviour
+    {
+        public void Start()
+        {
+            Harmony har = new Harmony("tinygrox.ChinesePatches");
+            List<AssemblyLoader.LoadedAssembly> assemblies = new List<AssemblyLoader.LoadedAssembly>();
+            foreach (AssemblyLoader.LoadedAssembly loadedAssembly in AssemblyLoader.loadedAssemblies)
+            {
+                assemblies.Add(loadedAssembly);
+            }
+            if (AssemblyLoader.loadedAssemblies.Any(a => a.assembly.GetName().Name == "WhereCanIGo"))
+            {
+                Debug.Log("Found Where Can I Go!");
+                har.Patch(
+                    //original: AccessTools.TypeByName("WhereCanIGo.WhereCanIGoEditor").GetMethod("GenerateDialog", BindingFlags.NonPublic),
+                    original: AccessTools.Method(AccessTools.TypeByName("WhereCanIGo.WhereCanIGoEditor"), "GenerateDialog"),
+                    transpiler: new HarmonyMethod(typeof(WhereCanIGoPatches), nameof(WhereCanIGoPatches.GenerateDialogLocPatch)));
+                har.Patch(
+                    original: AccessTools.Method(AccessTools.TypeByName("WhereCanIGo.WhereCanIGoEditor"), "GetDeltaVString"),
+                    transpiler: new HarmonyMethod(typeof(WhereCanIGoPatches), nameof(WhereCanIGoPatches.GenerateDialogLocPatch)));
+
+                har.Patch(
+                    original: AccessTools.Method(AccessTools.TypeByName("WhereCanIGo.WhereCanIGoFlight"), "GenerateDialog"),
+                    transpiler: new HarmonyMethod(typeof(WhereCanIGoPatches), nameof(WhereCanIGoPatches.GenerateDialogLocPatch)));
+                har.Patch(
+                    original: AccessTools.Method(AccessTools.TypeByName("WhereCanIGo.WhereCanIGoFlight"), "GetDeltaVString"),
+                    transpiler: new HarmonyMethod(typeof(WhereCanIGoPatches), nameof(WhereCanIGoPatches.GenerateDialogLocPatch)));
+            }
+            Destroy(this);
+        }
+    }
+}
