e93c4a6f9b95347135af9c6169fe92d5e50ea691
tinygrox <tinygrox@gmail.com>
2024-10-14 20:51:42 +0800

    漏了一处

--- Source/KSP_Chinese_Patches/CNPatchInit.cs
+++ Source/KSP_Chinese_Patches/CNPatchInit.cs
@@ -423,6 +423,11 @@ namespace KSP_Chinese_Patches
                 Debug.Log("\t[KSPCNPatches] [[x] Science!]StatusWindow_DrawWindowContents_Patch 已应用！");
 
                 har.Patch(
+                    original: AccessTools.Method(AccessTools.TypeByName("ScienceChecklist.StatusWindow"), "MakeSituationToolTip"),
+                    transpiler: new HarmonyMethod(typeof(xSciencePatches), nameof(xSciencePatches.StatusWindow_MakeSituationToolTip_Patch)));
+                Debug.Log("\t[KSPCNPatches] [[x] Science!]StatusWindow_MakeSituationToolTip_Patch 已应用！");
+
+                har.Patch(
                     original: AccessTools.Constructor(AccessTools.TypeByName("ScienceChecklist.xResourceData"), new[] { typeof(string) }),
                     transpiler: new HarmonyMethod(typeof(xSciencePatches), nameof(xSciencePatches.ResourcesName_Patch)));
                 Debug.Log("\t[KSPCNPatches] [[x] Science!]ResourcesName_Patch 已应用！");
