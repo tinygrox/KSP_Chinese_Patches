11b8302a2c52f68a691ddf0578e9cdff8106dce4
tinygrox <tinygrox@gmail.com>
2026-07-23 06:33:02 +0800

    refactor: 统一命名空间并将补丁类改为顶层
    将多个补丁文件从块级 namespace 改为 file-scoped namespace 并移除原来的嵌套类结构（如 HarmonyInitialise、各 ModPatches 等），使类成为顶层类型；在 HarmonyInitialise 中添加了程序集属性（KSPAssembly、KSPAssemblyDependency）并将 KSPAddon 标注移动到类型级别；统一调试输出为 UnityEngine.Debug.Log，整理并规范了 Patches 集合的初始化与方法布局；新增 CallerArgumentExpressionAttribute.cs。此提交主要为代码重构与格式现代化，便于 Harmony 注册与补丁管理，功能逻辑未做实质性变更。

--- Source/KSP_Chinese_Patches/ModPatches/ResearchBodiesPatches.cs
+++ Source/KSP_Chinese_Patches/ModPatches/ResearchBodiesPatches.cs
@@ -3,38 +3,37 @@ using KSP_Chinese_Patches.PatchesInfo;
 using System.Collections.Generic;
 using System.Reflection.Emit;
 
-namespace KSP_Chinese_Patches.ModPatches
+namespace KSP_Chinese_Patches.ModPatches;
+
+public class ResearchBodiesPatches : AbstractPatchBase
 {
-    public class ResearchBodiesPatches : AbstractPatchBase
+    public override string PatchName => "ResearchBodies";
+
+    public override string PatchDLLName => "ResearchBodies";
+
+    private static IEnumerable<CodeInstruction> ResearchBodies_FoundBody_Patch(IEnumerable<CodeInstruction> codeInstructions)
     {
-        public override string PatchName => "ResearchBodies";
+        CodeMatcher matcher = new CodeMatcher(codeInstructions).Start();
 
-        public override string PatchDLLName => "ResearchBodies";
+        matcher
+            .MatchStartForward(new CodeMatch(OpCodes.Ldstr, "Added "))
+            .SetOperandAndAdvance("发现天体！已添加 ")
+            .MatchStartForward(new CodeMatch(OpCodes.Ldstr, " science points !"))
+            .SetOperandAndAdvance(" 科学点数！")
+            ;
+        return matcher.InstructionEnumeration();
+    }
 
-        private static IEnumerable<CodeInstruction> ResearchBodies_FoundBody_Patch(IEnumerable<CodeInstruction> codeInstructions)
+    protected override void LoadAllPatchInfo()
+    {
+        Patches = new HashSet<HarPatchInfo>
         {
-            CodeMatcher matcher = new CodeMatcher(codeInstructions).Start();
-
-            matcher
-                .MatchStartForward(new CodeMatch(OpCodes.Ldstr, "Added "))
-                .SetOperandAndAdvance("发现天体！已添加 ")
-                .MatchStartForward(new CodeMatch(OpCodes.Ldstr, " science points !"))
-                .SetOperandAndAdvance(" 科学点数！")
-                ;
-            return matcher.InstructionEnumeration();
-        }
-
-        protected override void LoadAllPatchInfo()
-        {
-            Patches = new HashSet<HarPatchInfo>
-            {
-                new HarPatchInfo
-                (
-                    AccessTools.Method(AccessTools.TypeByName("ResearchBodies.ResearchBodiesController"), "FoundBody", new[] { typeof(int), typeof(CelestialBody), typeof(bool).MakeByRefType(), typeof(CelestialBody).MakeByRefType() }),
-                    new HarmonyMethod(typeof(ResearchBodiesPatches), nameof(ResearchBodiesPatches.ResearchBodies_FoundBody_Patch)),
-                    HarmonyPatchType.Transpiler
-                )
-            };
-        }
+            new HarPatchInfo
+            (
+                AccessTools.Method(AccessTools.TypeByName("ResearchBodies.ResearchBodiesController"), "FoundBody", new[] { typeof(int), typeof(CelestialBody), typeof(bool).MakeByRefType(), typeof(CelestialBody).MakeByRefType() }),
+                new HarmonyMethod(typeof(ResearchBodiesPatches), nameof(ResearchBodiesPatches.ResearchBodies_FoundBody_Patch)),
+                HarmonyPatchType.Transpiler
+            )
+        };
     }
 }
--- Source/KSP_Chinese_Patches/ModPatches/ThroughTheEyesPatches.cs
+++ Source/KSP_Chinese_Patches/ModPatches/ThroughTheEyesPatches.cs
@@ -4,61 +4,60 @@ using System;
 using System.Collections.Generic;
 using System.Reflection.Emit;
 
-namespace KSP_Chinese_Patches.ModPatches
+namespace KSP_Chinese_Patches.ModPatches;
+
+public class ThroughTheEyesPatches : AbstractPatchBase
 {
-    public class ThroughTheEyesPatches : AbstractPatchBase
+    public override string PatchName => "Through The Eyes of a Kerbal";
+
+    public override string PatchDLLName => "ThroughTheEyes";
+
+    private static IEnumerable<CodeInstruction> EVABoundFix_Hook_Patch(IEnumerable<CodeInstruction> codeInstructions, ILGenerator generator)
     {
-        public override string PatchName => "Through The Eyes of a Kerbal";
+        CodeMatcher matcher = new CodeMatcher(codeInstructions).Start();
 
-        public override string PatchDLLName => "ThroughTheEyes";
+        Label label1 = generator.DefineLabel();
+        Label label2 = generator.DefineLabel();
+        matcher
+            .MatchStartForward(new CodeMatch(OpCodes.Ldarg_0))
+            .AddLabels(new[] { label1 })
+            .MatchStartForward(new CodeMatch(OpCodes.Stloc_0))
+            .AddLabels(new[] { label2 })
+            .Start();
 
-        private static IEnumerable<CodeInstruction> EVABoundFix_Hook_Patch(IEnumerable<CodeInstruction> codeInstructions, ILGenerator generator)
-        {
-            CodeMatcher matcher = new CodeMatcher(codeInstructions).Start();
-
-            Label label1 = generator.DefineLabel();
-            Label label2 = generator.DefineLabel();
-            matcher
-                .MatchStartForward(new CodeMatch(OpCodes.Ldarg_0))
-                .AddLabels(new[] { label1 })
-                .MatchStartForward(new CodeMatch(OpCodes.Stloc_0))
-                .AddLabels(new[] { label2 })
-                .Start();
-
-            matcher
-                .InsertAndAdvance(
+        matcher
+            .InsertAndAdvance(
                 new CodeInstruction(OpCodes.Ldarg_0),
                 new CodeInstruction(OpCodes.Ldnull),
                 new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(UnityEngine.Object), "op_Inequality", new[] { typeof(UnityEngine.Object), typeof(UnityEngine.Object) })),
                 new CodeInstruction(OpCodes.Brtrue_S, label1),
                 new CodeInstruction(OpCodes.Ldnull),
                 new CodeInstruction(OpCodes.Br_S, label2)
-                )
-                ;
-            return matcher.InstructionEnumeration();
-        }
+            )
+            ;
+        return matcher.InstructionEnumeration();
+    }
 
-        public override bool IsModLoaded
+    public override bool IsModLoaded
+    {
+        get
         {
-            get
-            {
-                if (!StaticMethods.IsAssemblyLoaded("ThroughTheEyes", new Version(2, 0, 4)))
-                    return false;
-                return true;
-            }
+            if (!StaticMethods.IsAssemblyLoaded("ThroughTheEyes", new Version(2, 0, 4)))
+                return false;
+            return true;
         }
+    }
 
-        protected override void LoadAllPatchInfo()
+    protected override void LoadAllPatchInfo()
+    {
+        Patches = new HashSet<HarPatchInfo>
         {
-            Patches = new HashSet<HarPatchInfo>
-            {
-                new HarPatchInfo
-                (
-                    AccessTools.Method(AccessTools.TypeByName("FirstPerson.EVABoundFix"), "Hook"),
-                    new HarmonyMethod(typeof(ThroughTheEyesPatches), nameof(ThroughTheEyesPatches.EVABoundFix_Hook_Patch)),
-                    HarmonyPatchType.Transpiler
-                )
-            };
-        }
+            new HarPatchInfo
+            (
+                AccessTools.Method(AccessTools.TypeByName("FirstPerson.EVABoundFix"), "Hook"),
+                new HarmonyMethod(typeof(ThroughTheEyesPatches), nameof(ThroughTheEyesPatches.EVABoundFix_Hook_Patch)),
+                HarmonyPatchType.Transpiler
+            )
+        };
     }
 }
